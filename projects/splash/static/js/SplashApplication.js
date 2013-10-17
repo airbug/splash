32abe588192d0a39a3bc9960c8aacb2f89455f75
Brian Neisler <neisler.brian@gmail.com>
2013-10-17 16:53:08 -0700

    Update Splash project to use refactored bugioc. Update to use twitter bootstrap 3

--- projects/splash/static/js/SplashApplication.js
+++ projects/splash/static/js/SplashApplication.js
@@ -9,8 +9,10 @@
 
 //@Require('Class')
 //@Require('Obj')
-//@Require('bugioc.AutowiredScan')
+//@Require('bugioc.ConfigurationAnnotationProcessor')
 //@Require('bugioc.ConfigurationScan')
+//@Require('bugioc.ModuleAnnotationProcessor')
+//@Require('bugioc.ModuleScan')
 //@Require('bugioc.IocContext')
 
 
@@ -25,11 +27,13 @@ var bugpack = require('bugpack').context();
 // BugPack
 //-------------------------------------------------------------------------------
 
-var Class               = bugpack.require('Class');
-var Obj                 = bugpack.require('Obj');
-var AutowiredScan       = bugpack.require('bugioc.AutowiredScan');
-var ConfigurationScan   = bugpack.require('bugioc.ConfigurationScan');
-var IocContext          = bugpack.require('bugioc.IocContext');
+var Class                               = bugpack.require('Class');
+var Obj                                 = bugpack.require('Obj');
+var IocContext                          = bugpack.require('bugioc.IocContext');
+var ConfigurationAnnotationProcessor    = bugpack.require('bugioc.ConfigurationAnnotationProcessor');
+var ConfigurationScan                   = bugpack.require('bugioc.ConfigurationScan');
+var ModuleAnnotationProcessor           = bugpack.require('bugioc.ModuleAnnotationProcessor');
+var ModuleScan                          = bugpack.require('bugioc.ModuleScan');
 
 
 //-------------------------------------------------------------------------------
@@ -55,19 +59,19 @@ var SplashApplication = Class.extend(Obj, {
          * @private
          * @type {IocContext}
          */
-        this.iocContext = new IocContext();
-
-        /**
-         * @private
-         * @type {AutowiredScan}
-         */
-        this.autowiredScan      = new AutowiredScan(this.iocContext);
+        this.iocContext         = new IocContext();
 
         /**
          * @private
          * @type {ConfigurationScan}
          */
-        this.configurationScan = new ConfigurationScan(this.iocContext);
+        this.configurationScan  = new ConfigurationScan(new ConfigurationAnnotationProcessor(this.iocContext));
+
+        /**
+         * @private
+         * @type {ModuleScan}
+         */
+        this.moduleScan         = new ModuleScan(new ModuleAnnotationProcessor(this.iocContext));
     },
 
 
@@ -79,8 +83,8 @@ var SplashApplication = Class.extend(Obj, {
      * @param {function(Error)} callback
      */
     start: function(callback) {
-        this.autowiredScan.scan();
-        this.configurationScan.scan();
+        this.configurationScan.scanAll();
+        this.moduleScan.scanAll();
         this.iocContext.process();
         this.iocContext.initialize(callback);
     }
